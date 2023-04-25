d04a46cf03a738f29bbacb876ca59a9ffbfb0ce2
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-04-25 20:30:17 +0530

    removed the whitespace trailing the string

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,8 @@ fn main() {
         process::exit(1);
     }
     for file in args {
-        let mut value: String =
-            fs::read_to_string(Path::new(file.as_str())).expect("Cannot read file");
-        value.insert_str(0, "\n");
+        let value: String = fs::read_to_string(Path::new(file.as_str())).expect("Cannot read file");
         out_string.push_str(value.as_str());
     }
-    println!("{}", out_string.trim_start());
+    println!("{}", out_string);
 }
